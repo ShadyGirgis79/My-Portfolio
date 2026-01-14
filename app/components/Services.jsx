cd8adc288adbf36b28d41ef218faaf5a93b91ae7
Shady Emad <shadygirgis79@gmail.com>
2026-01-14 22:03:47 +0200

    Services Components is Done

---
+++ app/components/Services.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './Services.scss'
+import { serviceData } from './../../assets/assets'
+import Image from 'next/image'
+
+const Services = () => {
+  return (
+    <div id="services" className="services">
+      <h4 className="servicesIntro">What I offer</h4>
+      <h2 className="servicesTitle">My Services</h2>
+
+      <div className='serviceCards'>
+        {serviceData.map(({ icon, title, description, link }, index) => (
+          <div key={index} className="serviceCard">
+            <Image src={icon} alt="service icon" className="serviceIcon" />
+            <h3 className="serviceTitle">{title}</h3>
+            <p className="serviceDescription">{description}</p>        
+            </div>
+        ))}
+      </div>
+
+    </div>
+  )
+}
+
+export default Services
