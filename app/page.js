17845096099b3a3943640f432711c332b1d1b5b2
Shady Emad <shadygirgis79@gmail.com>
2026-01-23 17:58:50 +0200

    Contact section is Done

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Header from '../components/home/Header';
 import About from '../components/home/About';
 import Services from '../components/home/Services';
 import Work from '../components/home/Work';
+import Contact from '../components/home/Contact';
 
 const page = () => {
   return (
@@ -14,6 +15,7 @@ const page = () => {
       <About />
       <Services />
       <Work />
+      <Contact />
     </>
   )
 }
