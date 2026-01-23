b79f17b61fac41fa694b7ac7f5f7459aa586f59d
Shady Emad <shadygirgis79@gmail.com>
2026-01-23 17:57:52 +0200

    Improving assets file

--- assets/index.js
+++ assets/index.js
@@ -1,54 +1,52 @@
-import user_image from './user-image.jpeg';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import language_icon_dark from './language_icon_dark.png';
-import language_icon_light from './language_icon_light.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.png';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import mobile_background from './mobile-background.png';
-import web_dev_icon from './web-dev-icon.png';
-import ai_icon from './ai-icon.png';
-import flutter_icon from './flutter-icon.png';
-import react_icon from './react-icon.png';
-import arrow_down from './arrow-down.png';
-import arrow_up from './arrow-up.png';
-import linkedin_logo from './linkedin-logo.png';
-import github_logo from './github-logo.png';
-import whatsapp_logo from './whatsapp-logo.png';
-import email_logo from './email-logo.png';
-import cloud_download from './cloud-download-icon.png';
+import user_image from './images/user-image.jpeg';
+import code_icon from './icons/code-icon.png';
+import code_icon_dark from './icons/code-icon-dark.png';
+import edu_icon from './icons/edu-icon.png';
+import language_icon_dark from './icons/language_icon_dark.png';
+import language_icon_light from './icons/language_icon_light.png';
+import edu_icon_dark from './icons/edu-icon-dark.png';
+import project_icon from './icons/project-icon.png';
+import project_icon_dark from './icons/project-icon-dark.png';
+import vscode from './icons/vscode.png';
+import firebase from './icons/firebase.png';
+import figma from './icons/figma.png';
+import git from './icons/git.png';
+import mongodb from './icons/mongodb.png';
+import right_arrow_white from './icons/right-arrow-white.png';
+import mail_icon from './icons/mail_icon.png';
+import mail_icon_dark from './icons/mail_icon_dark.png';
+import profile_img from './images/profile-img.png';
+import download_icon from './icons/download-icon.png';
+import hand_icon from './icons/hand-icon.png';
+import header_bg_color from './images/header-bg-color.png';
+import moon_icon from './icons/moon_icon.png';
+import sun_icon from './icons/sun_icon.png';
+import arrow_icon from './icons/arrow-icon.png';
+import arrow_icon_dark from './icons/arrow-icon-dark.png';
+import menu_black from './icons/menu-black.png';
+import menu_white from './icons/menu-white.png';
+import close_black from './icons/close-black.png';
+import close_white from './icons/close-white.png';
+import web_icon from './icons/web-icon.png';
+import mobile_icon from './icons/mobile-icon.png';
+import ui_icon from './icons/ui-icon.png';
+import graphics_icon from './icons/graphics-icon.png';
+import right_arrow from './icons/right-arrow.png';
+import send_icon from './icons/send-icon.png';
+import right_arrow_bold from './icons/right-arrow-bold.png';
+import right_arrow_bold_dark from './icons/right-arrow-bold-dark.png';
+import mobile_background from './images/mobile-background.png';
+import web_dev_icon from './icons/web-dev-icon.png';
+import ai_icon from './icons/ai-icon.png';
+import flutter_icon from './icons/flutter-icon.png';
+import react_icon from './icons/react-icon.png';
+import arrow_down from './icons/arrow-down.png';
+import arrow_up from './icons/arrow-up.png';
+import linkedin_logo from './icons/linkedin-logo.png';
+import github_logo from './icons/github-logo.png';
+import whatsapp_logo from './icons/whatsapp-logo.png';
+import email_logo from './icons/email-logo.png';
+import cloud_download from './icons/cloud-download-icon.png';
 
 export const assets = {
     user_image,
@@ -64,8 +62,6 @@ export const assets = {
     git,
     mongodb,
     right_arrow_white,
-    logo,
-    logo_dark,
     mail_icon,
     mail_icon_dark,
     profile_img,
